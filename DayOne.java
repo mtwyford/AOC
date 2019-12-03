ba96b4aeb083c869c887478480d7ccdab62dfbf9
Marcus T <mtwyford@users.noreply.github.com>
2019-12-02 20:52:02 -0500

    Update DayOne.java

--- DayOne.java
+++ DayOne.java
@@ -1,1 +1,19 @@
 
+public class DayOne {
+  
+ public static void main(String[] args) {
+   double total=0;
+   
+   //take its mass, divide by three, round down, and subtract 2.
+  
+   for each item in file
+     mass = current line
+   //read in a value from a file, assuming a mass, 
+   //then add to a total the floor(mass/3) -2
+     total += (Math.floor(mass/3) -2);
+   //end for
+   
+  //print or return total
+    System.out.println(total);
+  
+}
